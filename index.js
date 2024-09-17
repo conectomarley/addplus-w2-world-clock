f18ed837fe1fa83e9225172d232276fb14a05ff3
Luana Marley <123222844+conectomarley@users.noreply.github.com>
2024-09-17 19:16:51 +1000

    update sao paulo html

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let saoPauloElement = document.querySelector("#sao-paulo");
 let saoPauloDateElement = saoPauloElement.querySelector(".date");
 let saoPauloTimeElement = saoPauloElement.querySelector(".time");
-saoPauloDateElement.innerHTML = moment().format("MMMM Do YYYY");
+let saoPauloTime = moment();
+
+saoPauloDateElement.innerHTML = saoPauloTime.format("MMMM Do YYYY");
 saoPauloTimeElement.innerHTML = "6:57:25 <small>PM</small>";
