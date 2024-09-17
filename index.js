98b6a04d0c86e11243d91db922536e1895e91e31
Luana Marley <123222844+conectomarley@users.noreply.github.com>
2024-09-17 21:21:56 +1000

    fixed the cdn and update sydney

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-let saoPauloElement = document.querySelector("#sao-paulo");
-let saoPauloDateElement = saoPauloElement.querySelector(".date");
-let saoPauloTimeElement = saoPauloElement.querySelector(".time");
-let saoPauloTime = moment().tz("America/Sao_Paulo");
+setInterval(function () {
+  let saoPauloElement = document.querySelector("#sao-paulo");
+  let saoPauloDateElement = saoPauloElement.querySelector(".date");
+  let saoPauloTimeElement = saoPauloElement.querySelector(".time");
+  let saoPauloTime = moment().tz("America/Sao_Paulo");
 
-saoPauloDateElement.innerHTML = saoPauloTime.format("MMMM Do YYYY");
-saoPauloTimeElement.innerHTML = saoPauloTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  saoPauloDateElement.innerHTML = saoPauloTime.format("dddd, MMMM Do YYYY");
+  saoPauloTimeElement.innerHTML = saoPauloTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
+
+setInterval(function () {
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("dddd, MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
