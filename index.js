5e52a588746e77cb1afda343d0209ff0408d30e8
Luana Marley <123222844+conectomarley@users.noreply.github.com>
2024-09-17 19:08:45 +1000

    update moment cdn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let saoPauloElement = document.querySelector("#sao-paulo");
-let saoPauloDateElement = document.querySelector("#sao-paulo.date");
-saoPauloDateElement.innerHTML = "hey";
+let saoPauloDateElement = saoPauloElement.querySelector(".date");
+let saoPauloTimeElement = saoPauloElement.querySelector(".time");
+saoPauloDateElement.innerHTML = moment().format("MMMM Do YYYY");
+saoPauloTimeElement.innerHTML = "6:57:25 <small>PM</small>";
